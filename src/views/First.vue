09968ccfda0f1fb669188077fabc77e6aeb5d684
chauncy <chauncy_xyz@outlook.com>
2023-10-31 16:31:08 +0800

    commit

---
+++ src/views/First.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import { reactive,ref } from 'vue';
+
+    const counter = reactive({count:0})
+    counter.count++
+    const message = ref('hello world!!!')
+    message.value = 'Changed'
+</script>
+<template>
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <p>count is : {{ counter }}</p>
+</template>
