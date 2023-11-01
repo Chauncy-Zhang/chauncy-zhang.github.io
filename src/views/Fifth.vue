69f0e2719e0ff679e0907ce52acb7b3d24173603
chauncy <chauncy_xyz@outlook.com>
2023-11-01 10:28:42 +0800

    commit

--- src/views/Fifth.vue
+++ src/views/Fifth.vue
@@ -2,12 +2,24 @@
 import { ref } from 'vue';
 
 const awesome = ref(true)
+const div1 = ref("div1")
+
 function toggle() {
     awesome.value = !awesome.value
 }
+
 </script>
 <template>
-    <button @click="toggle">toggle</button>
-    <h1 v-if="awesome">Vue is awesome!</h1>
-    <h1 v-else>Oh no 😢</h1>
+    <div :class="div1">
+        <button @click="toggle">toggle</button>
+        <h1 v-if="awesome">Vue is awesome!</h1>
+        <h1 v-else>Oh no 😢</h1>
+    </div>
 </template>
+<style>
+    .div1 {
+        /* margin-right: auto; */
+        /* width: 500px; */
+        color: aqua;
+    }
+</style>
